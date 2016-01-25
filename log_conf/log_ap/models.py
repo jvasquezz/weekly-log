fcf409a5d278fb261fc8351af8c9a48a515770a6
Ubuntu <ubuntu@ip-172-31-26-60.us-west-1.compute.internal>
2016-01-25 04:11:58 +0000

    forms, templates, views(modif)

--- log_conf/log_ap/models.py
+++ log_conf/log_ap/models.py
@@ -3,3 +3,10 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class updateHours(models.Model):
+    hours = models.TimeField()
+    
+    def __str__(self):
+        self.hours
+
+
